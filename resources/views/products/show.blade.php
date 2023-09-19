cfe30e5955a736697078a5981c16029a9ca4c36a
alberto <scontrerasa3@miumg.edu.gt>
2023-09-18 19:47:21 -0600

    Primer commit

---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,41 @@
+@extends('products.layout')
+  
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Mostrar productos</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('products.index') }}"> Regresar</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nombre:</strong>
+                {{ $product->name }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Cantidad:</strong>
+                {{ $product->cant }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Precio:</strong>
+                {{ $product->price }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Detalles:</strong>
+                {{ $product->detail }}
+            </div>
+        </div>
+    </div>
+@endsection
